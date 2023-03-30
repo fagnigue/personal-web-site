a7583ee09376eff5abd43be5b3a897a2cd4fb3fc
fagnigue <devfullcoul@gmail.com>
2023-03-30 13:30:05 +0000

    perf: set response headers to sendmail function

--- netlify/functions/sendmail.ts
+++ netlify/functions/sendmail.ts
@@ -24,6 +24,9 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
 
     return {
       statusCode: 200,
+      headers:{
+        'content-type': 'application/json; charset=utf-8'
+      },
       body: JSON.stringify({
         status: mailStatus
       })
